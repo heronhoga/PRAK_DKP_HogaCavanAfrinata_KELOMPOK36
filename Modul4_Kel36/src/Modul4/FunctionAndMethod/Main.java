422dc819586fd6d0d2dbb5a269da932dc78076f0
Hoga Cavan Afrinata <91598145+heronhoga@users.noreply.github.com>
2022-03-26 20:25:59 +0700

    hoga just added project

---
+++ Modul4_Kel36/src/Modul4/FunctionAndMethod/Main.java
@@ -0,0 +1,19 @@
+package Modul4.FunctionAndMethod;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            String pilih = null;
+            //Bagian Vincent (Buat Tampilan utama dan exit)
+
+    //Bagian Feri
+
+    //Bagian Ryan
+
+
+
